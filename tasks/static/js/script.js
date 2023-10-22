a487db35cac835e382e3f6a908431b94a2c54bbe
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-10-22 17:50:35 -0300

    [add]: paginação

--- tasks/static/js/script.js
+++ tasks/static/js/script.js
@@ -1,1 +1,14 @@
-console.log("funcionou!");
+$(document).ready(function () {
+  var deleteBtn = $(".delete-btn");
+
+  $(deleteBtn).on("click", function (e) {
+    e.preventDefault();
+
+    var delLink = $(this).attr("href");
+    var result = confirm("Quer deletar essa tarefa?");
+
+    if (result) {
+      window.location.href = delLink;
+    }
+  });
+});
